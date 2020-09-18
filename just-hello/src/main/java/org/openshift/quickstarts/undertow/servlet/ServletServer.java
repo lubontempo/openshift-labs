872df264b472f06e85dc5e832bee1c841dab2bd2
Luciana Bontempo <lubontempo@gmail.com>
2020-09-18 02:10:49 -0300

    Testando novas apps

--- just-hello/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
+++ just-hello/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
@@ -54,7 +54,7 @@ public class ServletServer {
                     .setDeploymentName("test.war")
                     .addServlets(
                             servlet("MessageServlet", MessageServlet.class)
-                                    .addInitParam("message", "Ol&aacute; Nelson, Alexandre e Lucas!! <br/> Como estamos at&eacute; agora? :-) ")
+                                    .addInitParam("message", "Olaaa Nelson, Alexandre e Lucas!! Como estamos ate agora? :-) ")
                                     .addMapping("/*"),
                             servlet("MyServlet", MessageServlet.class)
                                     .addInitParam("message", "MyServlet")
